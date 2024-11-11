68ab078c1ca67b5118c2b7a2bb1a673270fd466e
jack <juesc007@gmail.com>
2024-11-11 15:32:48 +0900

    Refactor avatar profile component

--- lib/components/avatar/avatar_profile.dart
+++ lib/components/avatar/avatar_profile.dart
@@ -1,54 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:farmers_journal/firestorage_service.dart';
 
 /// TODO
-/// 1. consider the case when the device is offline. (backgroundImage - imageProvider)
 /// 2. consider the user has update the profile image. (foreground image url must change)
-class AvatarProfile extends StatefulWidget {
+class AvatarProfile extends ConsumerWidget {
   final double width;
   final double height;
 
   const AvatarProfile({
     super.key,
-    this.width = 30.0,
-    this.height = 30.0,
+    this.width = 10.0,
+    this.height = 10.0,
   });
 
   @override
-  State<StatefulWidget> createState() {
-    return _AvatarProfile();
-  }
-}
-
-class _AvatarProfile extends State<AvatarProfile> {
-  String? defaultProfileImageURL;
-  Future<void> _getDefaultProfileImageURL() async {
-    final storageRef = FirebaseStorage.instance.ref();
-    final String url = await storageRef.child('/farmer.png').getDownloadURL();
-
-    debugPrint(defaultProfileImageURL);
-
-    setState(() {
-      defaultProfileImageURL = url;
-      debugPrint(defaultProfileImageURL);
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _getDefaultProfileImageURL();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final AsyncValue<String> defaultImageURL = ref.watch(defaultImageProvider);
     return SizedBox(
-      width: widget.width,
-      height: widget.height,
+      width: width,
+      height: height,
       child: CircleAvatar(
-        foregroundImage: defaultProfileImageURL != null
-            ? NetworkImage(defaultProfileImageURL!)
-            : null,
+        backgroundImage: switch (defaultImageURL) {
+          AsyncData(:final value) => NetworkImage(value),
+          AsyncError() => const AssetImage('assets/avatars/default.png'),
+          _ => const AssetImage('assets/avatars/default.png'),
+        },
       ),
     );
   }
---
+++ lib/firestorage_service.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import 'package:firebase_storage/firebase_storage.dart';
+
+part 'firestorage_service.g.dart';
+
+@riverpod
+Future<String> defaultImage(Ref ref) async {
+  final storageRef = FirebaseStorage.instance.ref();
+  final String url = await storageRef.child('/farmer.png').getDownloadURL();
+  return url;
+}
