4ffbbaff1dfe11df425f99140b6bc8d0ded74425
jack <juesc007@gmail.com>
2024-11-12 19:03:39 +0900

    fix code

--- lib/pages/page_main.dart
+++ lib/pages/page_main.dart
@@ -1,13 +1,22 @@
+import 'package:farmers_journal/model/journal.dart';
 import 'package:flutter/material.dart';
 //Riverpod
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:farmers_journal/firestore_service.dart';
 import 'package:farmers_journal/providers.dart';
-import 'package:farmers_journal/model/user.dart';
 
 import 'package:farmers_journal/components/button/button_create_post.dart';
 import 'package:farmers_journal/components/button/button_status.dart';
+import 'package:farmers_journal/components/button/button_filter_date.dart';
 import 'package:farmers_journal/components/avatar/avatar_profile.dart';
 import 'package:farmers_journal/components/card/card_single.dart';
+import 'package:farmers_journal/components/carousel/carousel.dart';
+
+// enums
+import 'package:farmers_journal/enums.dart';
+
+// utils
+import 'package:farmers_journal/utils.dart';
 
 /// TODO:
 ///  - 1. Apply font. Pretandard
@@ -18,12 +27,8 @@ import 'package:farmers_journal/components/card/card_single.dart';
 ///  - 6. Add onTap / onClick callback to profile image directing to the profile setting page.
 class PageMain extends ConsumerWidget {
   const PageMain({super.key});
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final ThemeData theme = Theme.of(context);
-    final ColorScheme colorScheme = theme.colorScheme;
-
     return Scaffold(
       body: const SafeArea(
         child: Padding(
@@ -32,6 +37,10 @@ class PageMain extends ConsumerWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               _TopNavTemp(),
+              Align(
+                alignment: Alignment.centerRight,
+                child: ButtonFilterDate(),
+              ),
               Expanded(child: _Content()),
             ],
           ),
@@ -54,8 +63,10 @@ class _TopNavTemp extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        const Text("농사 일지",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+        const Text(
+          "농사 일지",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+        ),
         IntrinsicHeight(
           child: SizedBox(
             height: 55,
@@ -77,7 +88,7 @@ class _TopNavTemp extends StatelessWidget {
                   onClick: () => debugPrint("Clicked"),
                 ),
                 const Spacer(),
-                const AvatarProfile(width: 55, height: 55),
+                const AvatarProfile(width: 50, height: 50),
               ],
             ),
           ),
@@ -130,6 +141,21 @@ class _DefaultContent extends StatelessWidget {
 class _UserContent extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final dateFilter = ref.watch(dateFilterProvider);
+
+    Widget child = switch (dateFilter) {
+      DateView.day => const _DayView(),
+      DateView.week => const _WeekView(),
+      DateView.month => const _MonthView(),
+    };
+    return child;
+  }
+}
+
+class _DayView extends ConsumerWidget {
+  const _DayView({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
     final journals = ref.watch(journalProvider);
 
     return ListView(
@@ -142,15 +168,61 @@ class _UserContent extends ConsumerWidget {
                   padding: const EdgeInsets.only(top: 4.0),
                   child: Center(
                     child: CardSingle(
-                        title: journal.title,
-                        content: journal.content,
-                        createdAt: journal.createdAt,
-                        image: journal.image),
+                      journal: journal,
+                    ),
                   ),
                 )
             ],
           AsyncError() => [const Text("Oops! Something went wrong")],
           _ => [const CircularProgressIndicator()]
         });
+  }
+}
+
+class _WeekView extends ConsumerWidget {
+  const _WeekView({super.key});
+  // TODO:
+  // 1. Currently, MyCarousel takes all the journals and return a single carousel, sorted in descending order by createdAt.
+  // WeekView journals should be divided into weeks.
+  // dividing the journals should be as follows.
+  // - This week's journals
+  // - Last week's journals
+  // - journals 2 weeks ago from now.
+  // - journals 3 weeks ago from now.
+  // Consider including the dividing logic inside of the provider.
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final journals = ref.watch(journalProvider);
+    final Widget child = switch (journals) {
+      AsyncData(:final value) => LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+          List<WeeklyGroup<Journal>> sortedJournals =
+              CustomDateUtils.groupItemsByWeek(value);
+          return Column(
+            children: [
+              for (var items in sortedJournals)
+                Column(children: [
+                  Text(items.weekLabel),
+                  SizedBox(
+                      height: 200, child: MyCarousel(journals: items.items))
+                ])
+            ],
+          );
+        }),
+      AsyncError() => const Text("Something went wrong"),
+      _ => const CircularProgressIndicator()
+    };
+    return child;
+  }
+}
+
+class _MonthView extends ConsumerWidget {
+  const _MonthView({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final journals = ref.watch(journalProvider);
+
+    return const Text("Month view");
   }
 }
