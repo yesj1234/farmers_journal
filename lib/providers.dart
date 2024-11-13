5cc7fe0b73b773bf675250330b2fbbf8837a70c5
jack <juesc007@gmail.com>
2024-11-13 14:54:33 +0900

    Add provider observer

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -14,5 +15,44 @@ class DateFilter extends _$DateFilter {
 
   changeDateFilter(DateView view) {
     state = view;
+  }
+}
+
+class MyObserver extends ProviderObserver {
+  @override
+  void didAddProvider(
+    ProviderBase<Object?> provider,
+    Object? value,
+    ProviderContainer container,
+  ) {
+    debugPrint('Provider $provider was initialized with $value');
+  }
+
+  @override
+  void didDisposeProvider(
+    ProviderBase<Object?> provider,
+    ProviderContainer container,
+  ) {
+    debugPrint('Provider $provider was disposed');
+  }
+
+  @override
+  void didUpdateProvider(
+    ProviderBase<Object?> provider,
+    Object? previousValue,
+    Object? newValue,
+    ProviderContainer container,
+  ) {
+    debugPrint('Provider $provider updated from $previousValue to $newValue');
+  }
+
+  @override
+  void providerDidFail(
+    ProviderBase<Object?> provider,
+    Object error,
+    StackTrace stackTrace,
+    ProviderContainer container,
+  ) {
+    debugPrint('Provider $provider threw $error at $stackTrace');
   }
 }
