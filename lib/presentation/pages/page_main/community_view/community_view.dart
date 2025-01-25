81d74107abc765d9f56d36866c49732725e6de20
jack <juesc007@gmail.com>
2025-01-25 22:18:38 +0900

    Refactor. divide widgets into separate files

--- lib/presentation/pages/page_main/community_view/community_view.dart
+++ lib/presentation/pages/page_main/community_view/community_view.dart
@@ -1,18 +1,28 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:farmers_journal/domain/model/journal.dart';
+import 'package:farmers_journal/domain/model/user.dart';
 import 'package:farmers_journal/presentation/components/card/day_view_card.dart';
 import 'package:farmers_journal/presentation/controller/journal/pagination_controller.dart';
 import 'package:farmers_journal/presentation/controller/journal/pagination_state.dart';
 import 'package:farmers_journal/presentation/controller/user/community_view_controller.dart';
-import 'package:farmers_journal/presentation/controller/user/user_controller.dart';
 import 'package:farmers_journal/presentation/pages/page_main/day_view_shimmer.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CommunityView extends ConsumerWidget {
-  CommunityView({super.key});
-  final ScrollController scrollController = ScrollController();
+class CommunityView extends ConsumerStatefulWidget {
+  const CommunityView({super.key});
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<CommunityView> createState() => _CommunityViewState();
+}
+
+class _CommunityViewState extends ConsumerState<CommunityView> {
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
     scrollController.addListener(() {
       double maxScroll = scrollController.position.maxScrollExtent;
       double currentScroll = scrollController.position.pixels;
@@ -21,7 +31,16 @@ class CommunityView extends ConsumerWidget {
         ref.read(paginationControllerProvider.notifier).fetchNextBatch();
       }
     });
+  }
 
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Stack(children: [
       RefreshIndicator(
         onRefresh: () async {
@@ -108,36 +127,30 @@ class ItemsListBuilder extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final userInfo = ref.watch(communityViewControllerProvider);
+    ref.watch(communityViewControllerProvider);
     return SliverList(
       delegate: SliverChildBuilderDelegate(
         (context, index) {
+          final journalInfo = journals[index];
           return GestureDetector(
               onTap: () {
                 ref
                     .read(communityViewControllerProvider.notifier)
-                    .getUserById(id: journals[index].id!);
+                    .getUserById(id: journalInfo.writer!);
                 showDialog(
                     context: context,
                     builder: (context) {
-                      return Dialog(
-                        child: Container(
-                          width: MediaQuery.sizeOf(context).width * 0.7,
-                          height: MediaQuery.sizeOf(context).height * 0.5,
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              width: 5,
-                              color: Colors.black,
-                            ),
-                          ),
-                          child: userInfo.when(
-                            data: (info) {},
-                            loading: () {},
-                            error: (e, st) {},
-                            initial: () {},
-                          ),
-                        ),
-                      );
+                      return Consumer(builder: (context, ref, child) {
+                        final userInfo =
+                            ref.watch(communityViewControllerProvider);
+                        return userInfo.when(
+                          data: (info) => DataStateDialog(
+                              info: info, journalInfo: journals[index]),
+                          loading: () => const ShimmerLoadingStateDialog(),
+                          error: (e, st) => const ErrorStateDialog(),
+                          initial: () => const ShimmerLoadingStateDialog(),
+                        );
+                      });
                     });
               },
               child: _DayViewCard(journal: journals[index]));
@@ -264,5 +277,184 @@ class ScrollToTopButton extends StatelessWidget {
                 : const SizedBox.shrink(),
           );
         });
+  }
+}
+
+class DataStateDialog extends StatelessWidget {
+  final AppUser info;
+  final Journal journalInfo;
+
+  const DataStateDialog({
+    super.key,
+    required this.info,
+    required this.journalInfo,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: SizedBox(
+        width: MediaQuery.sizeOf(context).width * 0.7,
+        height: MediaQuery.sizeOf(context).height * 0.5,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage:
+                        CachedNetworkImageProvider(info.profileImage!),
+                  ),
+                  const SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        info.name!,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        info.nickName!,
+                        style: const TextStyle(
+                            color: Color.fromRGBO(0, 0, 0, 0.5)),
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                  const Icon(Icons.more_horiz),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: RichText(
+                textAlign: TextAlign.start,
+                text: TextSpan(
+                  text: '${journalInfo.plant}, ',
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: journalInfo.place,
+                      style: const TextStyle(
+                          color: Colors.black, fontWeight: FontWeight.normal),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                children: [
+                  if (journalInfo.title!.isNotEmpty) Text(journalInfo.title!),
+                  if (journalInfo.content!.isNotEmpty)
+                    Text(journalInfo.content!),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ShimmerLoadingStateDialog extends StatelessWidget {
+  const ShimmerLoadingStateDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: SizedBox(
+        width: MediaQuery.sizeOf(context).width * 0.7,
+        height: MediaQuery.sizeOf(context).height * 0.5,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Shimmer for profile row
+              Row(
+                children: [
+                  _shimmerCircle(size: 50),
+                  const SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _shimmerRectangle(width: 120, height: 16),
+                      const SizedBox(height: 8),
+                      _shimmerRectangle(width: 80, height: 14),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              // Shimmer for content placeholder
+              _shimmerRectangle(width: 200, height: 16),
+              const SizedBox(height: 10),
+              _shimmerRectangle(width: double.infinity, height: 14),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _shimmerRectangle({required double width, required double height}) {
+    return Shimmer.fromColors(
+      baseColor: Colors.grey[300]!,
+      highlightColor: Colors.grey[100]!,
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: Colors.grey[300],
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    );
+  }
+
+  Widget _shimmerCircle({required double size}) {
+    return Shimmer.fromColors(
+      baseColor: Colors.grey[300]!,
+      highlightColor: Colors.grey[100]!,
+      child: Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          color: Colors.grey[300],
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
+  }
+}
+
+class ErrorStateDialog extends StatelessWidget {
+  const ErrorStateDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Dialog(
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.error, color: Colors.red),
+            SizedBox(height: 10),
+            Text(
+              "Error occurred while loading data!",
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
